23b700eb8838eac6c91c8c2f558d2c6bb08e2c7b
almecha <nurzhanaitaliyev@gmail.com>
2024-12-23 18:15:34 +0100

    New way of development

---
+++ bms_fsm_v2/inc/BMS.h
@@ -0,0 +1,40 @@
+#ifndef _BMS_H
+#define _BMS_H
+
+#include "StateMachine.h"
+#include <stdint.h>
+
+// BMS object structure
+typedef struct
+{
+
+    uint32_t state_of_charge;
+    uint32_t state_of_health;
+
+} BMS;
+
+// Event data structure
+typedef struct
+{
+    uint64_t v_max_id_rx, v_min_id_rx, v_max_rx, v_min_rx, v_mean_rx;
+    uint8_t ntc_temp;
+    uint16_t ntc_value;
+    uint8_t air_neg_cmd_is_active, air_neg_is_closed, air_neg_stg_mech_state_signal_is_active, air_pos_cmd_is_active,
+    air_pos_is_closed, air_pos_stg_mech_state_signal_is_active,
+    ams_err_is_active, dcbus_is_over60_v, dcbus_prech_rly_cmd_is_active, dcbus_prech_rly_is_closed,
+    imd_err_is_active, imp_dcbus_is_active, imp_any_is_active, imp_hv_relays_signals_is_active,
+    tsal_green_is_active;
+
+} BMSData;
+
+EVENT_DECLARE(BMS_IMD_Error_Event, BMSData);
+EVENT_DECLARE(BMS_AMS_Error_Event, BMSData);
+EVENT_DECLARE(BMS_CLOSE_AIR_NEG_REQ_Event, BMSData);
+EVENT_DECLARE(BMS_CLOSE_AIR_POS_REQ_Event, BMSData);
+EVENT_DECLARE(BMS_STOP_REQ_Event, BMSData);
+EVENT_DECLARE(BMS_CHARGE_REQ_Event, BMSData);
+EVENT_DECLARE(BMS_DRIVE_REQ_Event, BMSData);
+
+
+
+#endif // _BMS_H
